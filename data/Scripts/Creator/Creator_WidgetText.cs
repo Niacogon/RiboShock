950aef4bc53302531a27b21e8a915779828637ee
N7-Molot <curator39@gmail.com>
2024-04-29 23:00:04 +0500

    UI
    Первый UI в игре

---
+++ data/Scripts/Creator/Creator_WidgetText.cs
@@ -0,0 +1,52 @@
+using Unigine;
+using RiboShock.Systems;
+
+/// <summary>
+/// Раздел создателей
+/// </summary>
+namespace RiboShock.Creator {
+	/// <summary>
+	/// Виджет для текста - GUI
+	/// </summary>
+	[Component (PropertyGuid = "57f1a9a97eee3e3ddc4a7387020fb9b4f905f29a")]
+	public class Creator_WidgetText : System_GUI_Core {
+		/// <summary>
+		/// Виджет текста
+		/// </summary>
+		public WidgetLabel widgetLabel { private set; get; }
+
+		[ShowInEditor, Parameter (Title = "Текст виджета")]
+		private string widgetText = "";
+		[Parameter (Title = "Размер текста виджета")]
+		public int widgetTextSize = 20;
+		[Parameter (Title = "Обводка текста")]
+		public bool widgetTextOutline = false;
+
+		[Parameter (Title = "Виджет активен")]
+		public bool widgetActive = true;
+		
+		/// <summary>
+		/// Создание виджета
+		/// </summary>
+		public override void WidgetInit () {
+			widgetLabel = new WidgetLabel ();
+			widgetLabel.Text = widgetText;
+			widgetLabel.FontSize = widgetTextSize;
+			widgetLabel.FontOutline = widgetTextOutline ? 1 : 0;
+			widgetLabel.FontColor = widgetColor;
+			
+			widgetLabel.Arrange ();
+			widgetLabel.Hidden = !widgetActive;
+			gui.AddChild (widgetLabel, Gui.ALIGN_CENTER);
+			//Позиция
+			vec2 _targetPos = new vec2 (gui.Width * widgetPosition.x + widgetOffsetPosition.x, gui.Height * widgetPosition.y + widgetOffsetPosition.y);
+
+			if (widgetCentrPivot) {
+				_targetPos.x -= widgetLabel.Width;
+				_targetPos.y -= widgetLabel.Height;
+			}
+			
+			widgetLabel.SetPosition ((int) _targetPos.x, (int) _targetPos.y);
+		}
+	}
+}
