4f3deb23d705a7c86582cfafba106fd54ddf39c6
N7-Molot <curator39@gmail.com>
2024-04-29 20:29:25 +0500

    Комната разработчика
    Создана комната разработчика

---
+++ data/Scripts/Systems/System_ObjectRotation.cs
@@ -0,0 +1,15 @@
+using Unigine;
+
+namespace RiboShock.Systems {
+	[Component (PropertyGuid = "12eae39c619497ecc2a48e93417d3b53a075c817")]
+	public class System_ObjectRotation : Component {
+		[Parameter (Title = "Множитель", Tooltip = "Умножается ось для поворотц\n0 - нет поворота по оси")]
+		public vec3 rotationMultiply = new vec3 (0, 0, 1);
+		[Parameter (Title = "Скорость поворота")]
+		public float rotationSpeed = 1f;
+
+		void Update () {
+			node.Rotate (rotationSpeed * rotationMultiply.x, rotationSpeed * rotationMultiply.y, rotationSpeed * rotationMultiply.z);
+		}
+	}
+}
